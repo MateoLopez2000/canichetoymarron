ca3dbd58a9721d18e24bd6b5c9e8e3cbdc897fe5
Carly42 <carla.andrea.cbm@gmail.com>
2020-11-21 00:24:49 -0400

    Implementation of draggable marker and capturing longitude and latitude.

--- dueno/src/app/maps/maps.page.ts
+++ dueno/src/app/maps/maps.page.ts
@@ -19,6 +19,8 @@ export class MapsPage implements OnInit {
   lat: string;
   long: string;  
   location: any;
+
+  markers=[];
  
   constructor(
     private geolocation: Geolocation,
@@ -38,6 +40,8 @@ export class MapsPage implements OnInit {
   loadMap() {
     this.geolocation.getCurrentPosition().then((resp) => {
       let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.lat = resp.coords.latitude.toString();
+      this.long = resp.coords.longitude.toString();
       
       let mapOptions = {
         center: latLng,
@@ -53,9 +57,26 @@ export class MapsPage implements OnInit {
 
       this.map = new google.maps.Map(document.getElementById('map'), mapOptions); 
       
-      this.map.addListener('tilesloaded', () => {
-        this.lat = this.map.center.lat()
-        this.long = this.map.center.lng()
+      var marker = new google.maps.Marker({
+        draggable: true,
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+
+      this.markers.push(marker);
+      //alert(this.markers[0].position)
+
+      marker.addListener('dragend', () => {
+        this.markers.push(marker);
+        //alert(this.markers[this.markers.length-1].position);
+        var res = this.markers[this.markers.length-1].position.toString().split("(");
+        var res2= res[1].split(",");
+        //console.log(" lat::"+res2[0]);
+        var res3=res2[1].split(")")
+        //console.log("long::"+res3[0]);
+        this.lat = res2[0];
+        this.long = res3[0];
       }); 
     }).catch((error) => {
       console.log('Error getting location', error);
