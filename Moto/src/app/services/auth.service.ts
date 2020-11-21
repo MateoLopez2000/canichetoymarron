249c18984ee430ae7834999773cf8c1b1b5b6aab
MateoLopez2000 <lm67427572@gmail.com>
2020-11-20 22:23:49 -0400

    actualizado de la geolcalizacion al 70%

--- Moto/src/app/services/auth.service.ts
+++ Moto/src/app/services/auth.service.ts
@@ -1,42 +1,42 @@
-import { environment } from './../../environments/environment.prod';
-import { Injectable } from '@angular/core';
-import {AngularFireAuth} from "@angular/fire/auth";
+import { environment } from "./../../environments/environment.prod";
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from "@angular/router";
-import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
-import { promise } from 'protractor';
-import { resolve, reject } from 'q';
-import * as firebase from 'firebase';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from "@angular/fire/firestore";
+import { promise } from "protractor";
+import { resolve, reject } from "q";
+import * as firebase from "firebase";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
-  constructor(private AFauth : AngularFireAuth,
+  constructor(
+    private AFauth: AngularFireAuth,
     private router: Router,
-    private db: AngularFirestore) { }
+    private db: AngularFirestore
+  ) {}
 
-     user = this.AFauth.currentUser;
+  user = this.AFauth.currentUser;
 
-    set_location(nombre:string, latitud:string,longitud:string)
-    { return new Promise((resolve, reject) => {
-      let uid = "KV30xDgmayGKyLN1T2Wm"; 
-      this.db.collection('lugares').doc(uid).set({
-            uid: uid,
-            nombre: nombre,
-            latitud: latitud,
-            longitud: longitud
-          });
-        })  
-    }
-    update_location(nombre:string, latitud:string,longitud:string)
-    { return new Promise((resolve, reject) => {
-      let uid = "KV30xDgmayGKyLN1T2Wm"; 
-      this.db.collection('lugares').doc(uid).update({
-            nombre: nombre,
-            latitud: latitud,
-            longitud: longitud
-          });
-        })  
-    }
+  set_location(nombre: string, latitud: string, longitud: string) {
+    return new Promise((resolve, reject) => {
+      this.db.collection("lugares").doc(nombre).set({
+        nombreDeMoto: nombre,
+        latitud: latitud,
+        longitud: longitud,
+      });
+    });
   }
+  update_location(nombre: string, latitud: string, longitud: string) {
+    return new Promise((resolve, reject) => {
+      this.db.collection("lugares").doc(nombre).update({
+        latitud: latitud,
+        longitud: longitud,
+      });
+    });
+  }
+}
