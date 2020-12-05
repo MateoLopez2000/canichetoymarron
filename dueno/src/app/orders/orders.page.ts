cabfd99ddae31172792be7c522532ee73ec16ce2
vanebustillos <vanebustillos1@gmail.com>
2020-12-04 22:18:08 -0400

    Getting orders data and setting it in cards.

--- dueno/src/app/orders/orders.page.ts
+++ dueno/src/app/orders/orders.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from "../services/data/firestore.service";
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersPage implements OnInit {
 
-  constructor() { }
+  pedidos: any = [];
 
-  ngOnInit() {
+  constructor(
+    private firestoreService: FirestoreService
+  ) {
   }
 
+  ngOnInit() {
+    this.getPedidos();
+  }
+  getPedidos() {
+    this.firestoreService.getPedidos().subscribe((pedidosList) => {
+      this.pedidos = [];
+      pedidosList.forEach((pedido: any) => {
+        let pedidoData = pedido.payload.doc.data();
+        let idPedido = pedido.payload.doc.id.toUpperCase();
+        this.pedidos.push({
+          id: idPedido,
+          position: {
+            lat: pedidoData.position.lat,
+            lng: pedidoData.position.lng,
+          },
+          estado: pedidoData.estado,
+          direccion: pedidoData.direccion,
+          fechahorapedido: pedidoData.fechahorapedido,
+          nit: pedidoData.nit,
+          moto: this.getDriverName(pedidoData.moto)[0].toUpperCase(),
+          nombre: pedidoData.nombre,
+          productos: pedidoData.productos,
+          sucursal: pedidoData.sucursal,
+          telefono: pedidoData.telefono,
+          total: pedidoData.total
+        });
+      });
+    });
+  }
+  getDriverName(user: string){
+    return user.split('@');
+  }
 }
